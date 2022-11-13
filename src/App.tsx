bc897d343b7c15dfb35e697fabed9a431e385476
AndreasKoettl95 <a.koettl@hotmail.com>
2022-11-13 10:34:56 +0100

    Resolved merge conflicts.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import {Desktop} from "./Desktop";
 import Settings from './components/settings/settings';
-import Menu from './components/menu/menu';
 import './App.css';
 
 export const App = () => {
