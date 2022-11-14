5d2d5d34ea6318abdf0f239522031964e3e2452b
AndreasKoettl95 <a.koettl@hotmail.com>
2022-11-14 15:52:59 +0100

    Isolated browser styling.

--- src/components/applications/browser/AppBrowser.tsx
+++ src/components/applications/browser/AppBrowser.tsx
@@ -3,6 +3,8 @@ import './AppBrowser.css'
 
 export const AppBrowser = () => {
     return (
-        <iframe title="Browser" src="https://google.com/search?igu=1"></iframe>
+        <div className="browser-content">
+            <iframe title="Browser" src="https://google.com/search?igu=1"></iframe>
+        </div>
     );
 }
